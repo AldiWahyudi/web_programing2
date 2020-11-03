c72116b33dc8586a586a7fab508c28a252dc54d4
AldiWahyudi <wahyualddi727@gmail.com>
2020-11-03 22:31:14 +0700

    tugas

---
+++ README.md
@@ -0,0 +1,1 @@
+## Aldi Wahyudi 18090142 5A
---
+++ praktikum/pertemuan1/Perkenalan.php
@@ -0,0 +1,10 @@
+<?php
+class Perkenalan extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalan</h1>";
+		echo "Nama Saya Aldi Wahyudi<br>
+				Saya tinggal di Lebaksiu<br>";
+	}
+}
